ace469d2944edb300f3bb3abed2ccf902646f2b6
NextGenVishal <vishal.20215054@mnnit.ac.in>
2022-11-05 13:00:21 +0530

    initial commit

---
+++ app/src/main/java/gen/vishal/mercato/MainActivity.kt
@@ -0,0 +1,25 @@
+package gen.vishal.mercato
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.nextButton)
+
+        button.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+//        button.setOnClickListener {
+//           startActivity(Intent(this,LoginActivity::class.))
+//        }
+    }
+}
